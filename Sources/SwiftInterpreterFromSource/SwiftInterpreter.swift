79afe1c4828254b60b7e04c627c48cd3da72f28c
Joe Hinkle <joehinkle11@gmail.com>
2021-06-01 16:32:48 -0600

    fix

--- Sources/SwiftInterpreterFromSource/SwiftInterpreter.swift
+++ Sources/SwiftInterpreterFromSource/SwiftInterpreter.swift
@@ -1,6 +1,6 @@
 import Foundation
 import SwiftInterpreterSource
 
-public func interpret(_ astData: Data) throws -> Any {
+public func interpret(_ astData: [UInt8]) throws -> Any {
     return try SwiftInterpreterSource.interpret(astData)
 }
