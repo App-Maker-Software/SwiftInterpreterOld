4853ab427a60596cb6fc9b60c816f1e9dea9ae4c
Joe Hinkle <joehinkle11@gmail.com>
2021-07-01 23:07:14 -0600

    small fix

--- Sources/SwiftInterpreter/SwiftInterpreter.swift
+++ Sources/SwiftInterpreter/SwiftInterpreter.swift
@@ -5,6 +5,9 @@ import SwiftInterpreterSource
 #endif
 
 public func interpret(_ astData: [UInt8], using stack: Stack? = nil) throws -> Any {
-//    return try SwiftInterpreterBinary.interpret(astData, using: stack)
+    #if canImport(SwiftInterpreterBinary)
+    return try SwiftInterpreterBinary.interpret(astData, using: stack)
+    #else
     return try SwiftInterpreterSource.interpret(astData, using: stack)
+    #endif
 }
