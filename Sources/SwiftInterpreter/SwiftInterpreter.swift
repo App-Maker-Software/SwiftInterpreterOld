850bd249c64fc334c3a3926ecfa3e2c0a9368846
Joe Hinkle <joehinkle11@gmail.com>
2021-09-07 12:55:37 -0500

    fix

--- Sources/SwiftInterpreter/SwiftInterpreter.swift
+++ Sources/SwiftInterpreter/SwiftInterpreter.swift
@@ -1,15 +1,14 @@
 #if canImport(SwiftInterpreterBinary)
 import SwiftInterpreterBinary
+let libInterpret = SwiftInterpreterBinary.interpret
 #elseif canImport(SwiftInterpreterSource)
 import SwiftInterpreterSource
+let libInterpret = SwiftInterpreterSource.interpret
+#elseif canImport(SwiftInterpreterBinarySource)
+import SwiftInterpreterBinarySource
+let libInterpret = SwiftInterpreterBinarySource.interpret
 #endif
 
-#if canImport(SwiftInterpreterBinary) || canImport(SwiftInterpreterSource)
-public func interpret(_ astData: [UInt8], using stack: Stack? = nil) throws -> Any {
-    #if canImport(SwiftInterpreterBinary)
-    return try SwiftInterpreterBinary.interpret(astData, using: stack)
-    #else
-    return try SwiftInterpreterSource.interpret(astData, using: stack)
-    #endif
+public func interpret(_ astData: [UInt8], using stack: Stack? = nil) throws {
+    try libInterpret(astData, stack)
 }
-#endif
